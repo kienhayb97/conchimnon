e9436ea808ce02f1cd26ae9215d288383828c47f
kienhayb97 <kienhayb.21091997@gmail.com>
2020-04-02 02:52:12 +0700

    firstdfgt

--- src/app/component/admin/comment/comment.service.ts
+++ src/app/component/admin/comment/comment.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {IComment} from './IComment';
+// @ts-ignore
 import {environment} from '../../../../environments/environment';
 
 @Injectable({
